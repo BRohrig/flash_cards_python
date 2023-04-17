d66d0f0bf873f820b7dd2b28d5411c67bc336ab6
Brady Rohrig <brady.rohrig@gmail.com>
2023-04-17 09:44:15 -0700

    test: wrote tests for deck instance methods

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -6,4 +6,15 @@ from card import Card
 from deck import Deck
 
 card_1 = Card('What is the capital of Alaska?', 'Juneau', "Geography")
-card_2 = Card()
+card_2 = Card('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', "STEM")
+card_3 = Card('Describe in words the exact direction that is 697.5° clockwise from due north?','North north west', "STEM")
+cards = [card_1, card_2, card_3]
+deck = Deck(cards)
+
+class TestDeck(unittest.Testcase):
+  def test_count(self):
+    self.assertEqual(deck.count, 3)
+
+  def test_category(self):
+    self.assertEqual(deck.cards_in_category("STEM"), [card_2, card_3])
+    self.assertEqual(deck.cards_in_category("STUFF"), [])
