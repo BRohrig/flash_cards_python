0387397c654428455acc85e3cbd59ca721384f33
Brady Rohrig <brady.rohrig@gmail.com>
2023-04-21 14:33:45 -0700

    test: wrote test for percent_by_category method

--- tests/test_round.py
+++ tests/test_round.py
@@ -82,3 +82,14 @@ class TestRound(unittest.TestCase):
     round3.take_turn('West maybe')
     self.assertEqual(round3.turns_by_cat('STEM'), 2)
     self.assertEqual(round3.turns_by_cat("Geography"), 1)
+
+  def percent_by_cat(self):
+    cards = [card_1, card_2, card_3]
+    deck2 = Deck(cards)
+    round3 = Round(deck2)
+    round3.take_turn('Juneau')
+    round3.take_turn('the muffin man')
+    self.assertEqual(round3.percent_by_cat('STEM'), 0.0)
+    round3.take_turn('North north west')
+    self.assertEqual(round3.percent_by_cat('Geography'), 100.0)
+    self.assertEqual(round3.percent_by_cat('STEM'), 50.0)
